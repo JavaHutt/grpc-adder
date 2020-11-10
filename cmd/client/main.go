43df950b1ea6d0ae3b58101a5cc3b364da4cdbb1
JavaHutt <alxmer111@gmail.com>
2020-11-10 17:11:58 +0400

    client with grpc dial

---
+++ cmd/client/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"strconv"
+
+	"github.com/JavaHutt/grpc-adder/pkg/api"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	flag.Parse()
+
+	if flag.NArg() < 2 {
+		log.Fatal("not enough arguments!")
+	}
+
+	x, err := strconv.Atoi(flag.Arg(0))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	y, err := strconv.Atoi(flag.Arg(1))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conn, err := grpc.Dial(":8080", grpc.WithInsecure())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c := api.NewAdderClient(conn)
+	res, err := c.Add(context.Background(), &api.AddRequest{X: int32(x), Y: int32(y)})
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(res.GetResult())
+}
