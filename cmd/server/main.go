a024b4c64873b5cf29871564e5d0916c3733d455
JavaHutt <alxmer111@gmail.com>
2020-11-10 16:52:25 +0400

    listener for 8080

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+	"net"
+
 	"github.com/JavaHutt/grpc-adder/pkg/api"
 	"github.com/JavaHutt/grpc-adder/pkg/api/adder"
 	"google.golang.org/grpc"
@@ -10,4 +13,13 @@ func main() {
 	s := grpc.NewServer()
 	srv := &adder.GRPCServer{}
 	api.RegisterAdderServer(s, srv)
+
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = s.Serve(listener); err != nil {
+		log.Fatal(err)
+	}
 }
